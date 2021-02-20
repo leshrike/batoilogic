7cb76c2b3e4dd6a6fdf8edaacde4e15a93960933
leshrike <57752465+leshrike@users.noreply.github.com>
2021-02-20 22:48:27 +0100

    cambios api auth

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 /**
  * @OA\Post(
@@ -42,11 +41,12 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    public function login(Request $request){
+	public function login(Request $request)
+    {
         $usuario = User::where('email', $request->login)->first();
         if (!$usuario || !Hash::check($request->password, $usuario->password))
         {
-            return response()->json(['error' => 'Credenciales no válidas'], 401);
+            return response()->json(['error' => 'Credenciales no validas'], 401);
         }
         else
         {
